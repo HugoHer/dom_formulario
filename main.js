b9cf92e731f53b725572cdc8c4a888e1710b6428
Hugo Hernandez <nandezhugo86@gmail.com>
2022-08-10 12:44:29 -0500

    second commit

--- main.js
+++ main.js
@@ -6,5 +6,6 @@ formulario.addEventListener('submit', function(e){
 e.preventDefault();
 let name = document.getElementById('nombre').value 
 let secondName = document.getElementById('apellido').value
-console.log(`hola ${name} ${secondName}, gracias por rellenar el formulario de LarnU`)
+let texto =(`hola ${name} ${secondName}, gracias por rellenar el formulario de LarnU`)
+saludo.innerHTML= texto
 })
